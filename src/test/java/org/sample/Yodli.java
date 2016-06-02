188a3700cd1237450a93ddde1ede96efdfaff4b5
Gergely Gal <gergely_gal@epam.com>
2016-06-02 16:08:37 +0200

    Adding a really useful test

---
+++ src/test/java/org/sample/Yodli.java
@@ -0,0 +1,13 @@
+package org.sample;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Yodli {
+
+    @Test
+    public void doDaYodli() {
+        System.out.println("Yollllololeeeehooooooooooo!!!!!!!!");
+        Assert.assertTrue(true);
+    }
+}
